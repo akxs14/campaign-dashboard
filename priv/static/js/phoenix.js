b0308c747f0e9a45127618b5b6a1faa79b95a4d4
angelos <angelos.kapsimanis@gmail.com>
2014-09-08 23:52:11 +0200

    Added accordion chevron?

--- priv/static/js/phoenix.js
+++ priv/static/js/phoenix.js
@@ -315,16 +315,4 @@
     return exports;
   });
 
-  $(".openpanel").on("click", function() {
-    $("#panel3").collapse('show');
-  });
-  $(".closepanel").on("click", function() {
-    $("#panel3").collapse('hide');
-  });
-
-  /* ensure any open panels are closed before showing selected */
-  $('#accordion').on('show.bs.collapse', function () {
-    $('#accordion .in').collapse('hide');
-  });
-
 }).call(this);
