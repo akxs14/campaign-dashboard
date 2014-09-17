7afeabfb657d267d1878d3cd05d500c1c490870a
Angelos Kapsimanis <angelos@advancedtelematic.com>
2014-09-17 10:20:10 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,14 +4,19 @@ get '/' do
   "pipabu!"
 end
 
+# See all campaigns related to the user
 get '/campaigns' do
-
 end
 
+# submit a new campaign to generate the json,
+# launch the server 
+post '/campaigns' do
+end
+
+# show the new campaign page
 get '/campaigns/new' do
-
 end
 
+# show the panel with the campaign details
 get '/campaigns/:id' do
-
 end
