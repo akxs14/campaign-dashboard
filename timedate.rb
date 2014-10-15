c64438bb555a83e609ffc311780796a6f9f55c12
angelos <angelos.kapsimanis@gmail.com>
2014-10-15 22:39:27 +0200

    Added dynamic status light on /campaigns

--- timedate.rb
+++ timedate.rb
@@ -9,3 +9,10 @@ def remaining_days end_time, start_time
     (end_time - Time.now).to_i / ONE_DAY    
   end
 end
+
+def campaign_status end_time, start_time, error = 0
+  return "images/red.png" if error != 0
+  return "images/yellow.png" if start_time > Time.now
+  return "images/grey.png" if end_time < Time.now
+  return "images/green.png"
+end
